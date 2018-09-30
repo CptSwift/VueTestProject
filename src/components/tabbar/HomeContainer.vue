eae2e3b8960320b25d8151f61ef3e61b693d4548
CptSwift <chenzihao000@qq.com>
2018-09-30 16:01:37 +0800

    #comment 创建tabbar和路由

---
+++ src/components/tabbar/HomeContainer.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <mt-swipe v-for="item in pictureList" :key="item.url" :auto="4000">
+            <mt-swipe-item>
+                <img :src="item.img" alt="">
+            </mt-swipe-item>
+            <mt-swipe-item>2</mt-swipe-item>
+            <mt-swipe-item>3</mt-swipe-item>
+        </mt-swipe>
+
+        <h3>HomeContainer</h3>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return {
+            pictureList: []
+        }
+    },
+    methods:{
+        getPictureRoll(){
+//            this.$http.get().then(result => {
+//                console.log(result.body);
+//            });
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .mint-swipe{
+        height: 200px;
+
+        .mint-swipe-item{
+            background-color: red;
+        }
+
+        img{
+            width: 100%;
+            height: 100%;
+        }
+    }
+
+
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
 // 入口文件
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
 import app from './App.vue'
-import { Header, Button } from 'mint-ui'
+import router from './router'
+
+import { Header, Swipe, SwipeItem } from 'mint-ui'
+
 import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
 
 Vue.component(Header.name, Header)
-Vue.component(Button.name, Button)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
 
 var vm = new Vue({
     el: '#app',
-    render: c => c(app)
+    render: c => c(app),
+    router
 })
